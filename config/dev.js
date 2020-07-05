f0a0479f41b3ef6ae3ff86af8b443287d4c6bdbc
Voroshilov Max <voroshilovmax90@gmail.com>
2020-07-05 17:36:51 +0300

    init

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '980154335684-rb453l6a60oj5glemumrbq2vtbhk4ref.apps.googleusercontent.com',
+  googleClientSecret: 'M8IFVbVKugEO3m2md4UBi9ay',
+  mongoURI: '  mongodb+srv://Voroshilov:S691Py4DRm57aVs6@voroshilov-qrmk4.mongodb.net/<dbname>?retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
