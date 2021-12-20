d3c921bded52cfc039ea910811838cd9a2e49492
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-12-20 16:49:05 +0200

    finish

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,8 @@ module.exports = {
   googleClientID:
     '980154335684-rb453l6a60oj5glemumrbq2vtbhk4ref.apps.googleusercontent.com',
   googleClientSecret: 'M8IFVbVKugEO3m2md4UBi9ay',
-  mongoURI: '  mongodb+srv://Voroshilov:S691Py4DRm57aVs6@voroshilov-qrmk4.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://Voroshilov:S691Py4DRm57aVs6@voroshilov-qrmk4.mongodb.net/<dbname>?retryWrites=true&w=majority',
   cookieKey: '123123123',
+  accessKeyId: 'AKIAX7MFXFJLFXOVSHPC',
+  secretAccessKey: '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'
 };
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
+require("./services/cache");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -26,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
+require('./routes/uploadRoutes')(app);
 
 if (['production'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
